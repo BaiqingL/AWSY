c8ba23044c9de99c46ddebcfdcc71c23bb2f12d9
mathew <mathewhan01@gmail.com>
2019-10-19 16:11:58 -0400

    reverse address lookup code

---
+++ reverseAddressLookup.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+API_KEY = "5cec97f82b294b0cb0de3c11ddc9a76a"
+
+# Address format (list):  ["NUM STREET", "CITY", "STATE ZIP", "COUNTRY"]
+def sendRequest(address):
+    street = address[0]
+    city = address[1]
+    state, zipcode = address[2].split()
+    country = address[3]
+
+
+    parameters = {
+        "api_key": API_KEY,
+        "city": city,
+        "country_code": country,
+        "postal_code": zipcode,
+        "state_code": state,
+        "street_line_1": street
+    }
+
+    url = "https://api.ekata.com/3.0/location"
+
+    response = requests.get(url, params=parameters)
+    data = response.json()
+
+    return data
+
